407cafe9a8e173d2d859ac97ad130530d8b901ca
JCadiz <jesuscadiz22@gmail.com>
2018-10-23 16:54:17 -0400

    hay que seguir probando con el time

--- Proyecto1/timeprog.c
+++ Proyecto1/timeprog.c
@@ -7,20 +7,24 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]){
-//pid_t tipo de dato
-    pid_t pid=fork();
+//pid_t tipo de dato para el fork
+    pid_t pid = fork();
     clock_t start, end;
+    double seg;
 
     if (pid<0){
         printf("errno = %d\n", errno);
         perror("main");
     }
-    else if (pid == 0){
+    else if (pid == 0){ 
         start = clock();
         execv(argv[1], &argv[1]);
         end = clock();
-    }
-
-    printf("El programa se ejecuto en %ld segundos\n", end);
+	}
+	 seg = (double)(start - end) / CLOCKS_PER_SEC;  // ld
+    	printf("El programa se ejecuto en %f segundos\n", seg);
+	exit(0);
+    // }
+	
     return 0;
 }
