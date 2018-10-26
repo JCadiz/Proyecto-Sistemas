40edc0f5dd21070931244248e95297709c31adac
JCadiz <jesuscadiz22@gmail.com>
2018-10-26 17:22:00 -0400

    agregado el count.c

---
+++ Proyecto1/count.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main( int argc, char* argv[] )
+{
+    const int TOTAL = 10;
+    int i;	
+    
+    for( int i = 0; i < TOTAL; i++ )
+	printf( "%i\n", i );
+
+    return 0;
+}
