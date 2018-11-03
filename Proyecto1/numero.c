db2789fe46719206f3919b04b2acc6034be59eb3
JCadiz <jesuscadiz22@gmail.com>
2018-11-03 16:44:22 -0400

    terminado el ejercicio 2

--- Proyecto1/numero.c
+++ Proyecto1/numero.c
@@ -1,3 +1,7 @@
+#ifndef _POSIX_SOURCE
+#define _POSIX_SOURCE 1
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -5,36 +9,69 @@
 #include <errno.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <signal.h>
+
+void wakeup( int ); /* declaración */
+long count=0;
+bool cierre=true;
 
 int main( int argc, char **argv)
 {
 	pid_t id;
-	double count=0;
-	bool fin= true;
-	double p1,p2;
-	
-	 while(1 && fin){
-	    id = fork();
-		p1 = getpid();
+	int i=1;
+	bool entrada= true;
+	long p1;
 
-		if (id<0){ 
+	struct sigaction sigact;
+    	sigset_t sigset;
+    
+	/* montar el manejador para le interrupcion del teclado (DEL o CTRL-C) */
+	sigemptyset( &sigset ); /* conjunto vacio de mascaras de señales */
+	sigact.sa_handler = wakeup; /* registra el manejador de señales */
+	sigact.sa_mask = sigset; /* utilizar el sigset vacio */
+	sigact.sa_flags = 0; /* no se necesitan banderas o flags */
+	sigaction( SIGINT, &sigact, NULL );
+	
+	 while((i > 0) && cierre){
+	    id = fork();
+
+		if (id < 0){ 
 			printf("errno = %d\n", errno);
 			perror("main");
-			fin = false;
-	    	} else if ( id == 0 ){
-			p2 = getppid ();
+			cierre = false;
+			printf("El numero de maximo de procesos que un usuario puede tener es: %ld \n", count);
+	    	}else if ( id == 0 ){
+			
+			printf ("Inicio proceso hijo. proceso padre PID=%d, proceso hijo PPID=%d\n", getpid (), getppid ());
 			exit( 0 ); 
 		}else {
+		  
 		   id = wait( NULL );
-			if (p1>p2){
-				count = p1;
-			}else{
-				count = p2;
-			}
+		   // printf("El numero maximo de procesos que un usuario puede tener es: %i \n", count);
 		}
+
+		if(entrada == true){
+			count= p1;
+			entrada= false;
+		}else{
+			count++;
+		}
+								
+		
+		
+
 	 }
 	
-printf("El numero de maximo de procesos que un usuario puede tener es: %f \n", count);
-	
 	return 0;
+}
+
+void wakeup(int s )
+{
+	static int detener = 0;
+	if(detener == 0){
+		cierre=false;
+		printf("El numero de procesos alcanzado hasta el momento es: %ld \n", count);
+	}
+  	
+
 }
