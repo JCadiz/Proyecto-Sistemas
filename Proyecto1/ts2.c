4cd82a68404f0d961422ece969196752a9456fd1
adnavarro <alfonsonavarromedina@gmail.com>
2018-11-05 19:46:35 -0400

    Mejorado ts2

--- Proyecto1/ts2.c
+++ Proyecto1/ts2.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/times.h>
 #include <errno.h>
 
 int main(int argc, char *argv[]){
@@ -13,9 +14,11 @@ int main(int argc, char *argv[]){
     int uid2 = atoi(argv[3]); // id del segundo usuario
     int count2 = atoi(argv[4]); // numero de procesos que le segundo usuario debe bifurcar
     int timing = atoi(argv[5]); // segundos de duracion del programa
-    (double) timing integer;
+    (double) timing;
     pid_t pid1, pid2;
 	time_t start, end, total, startp, endp, totalp;
+    struct tms start_tms;
+    struct tms end_tms;
     printf("UID  COUNT  USERTIME  SYSTIME  USER+SYSTEM\n");
     start = time(NULL);
 
@@ -28,24 +31,30 @@ int main(int argc, char *argv[]){
     }
 
     if (pid1==0){
-        pid1 = wait(NULL);
+        pid1 = getpid ();
+		kill(pid1, SIGKILL);
     }
     else{
         startp = time(NULL);
+        times(&start_tms);
         while(1){
             end = time(NULL);
             total = (double)(end-start);
             if (timing == total) break;
 
         }
-        endup = time(NULL);
+        endp = time(NULL);
+        times(&end_tms);
+        clock_t stime = end_tms.tms_cstime - start_tms.tms_cstime;
         totalp = (double)(endp-startp);
-        printf("%d  %d  %ld  SYSTEMTIME  USER+SYSTEM\n", uid1 count1 totalp);
+        printf("%d  %d  %lld  %lu  USER+SYSTEM\n", uid1, count1, totalp, stime);
 
     }
 
     if (pid2==0){
         pid2 = wait(NULL);
+        //pid2= getpid ();
+		//kill(pid2, SIGKILL);
     }
     else{
         startp = time(NULL);
@@ -55,7 +64,7 @@ int main(int argc, char *argv[]){
             if (timing == total) break;
 
         }
-        endup = time(NULL);
+        endp = time(NULL);
         totalp = (double)(endp-startp);
     }
 
